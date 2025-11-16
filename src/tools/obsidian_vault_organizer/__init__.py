cc71b509283a0bc254c9483641cbf3dbd969350e
Sathyan <sathyan17.1980@gmail.com>
2025-11-16 10:36:51 -0500

    Create __init__.py

---
+++ src/tools/obsidian_vault_organizer/__init__.py
@@ -0,0 +1,5 @@
+"""Obsidian Vault Organizer Tool.
+
+Batch operations for organizing vault: move, tag, archive, delete notes.
+Consolidates what would be 3+ separate tools (note_mover, tag_manager, note_deleter).
+"""
